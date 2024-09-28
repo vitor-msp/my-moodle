196600f6531f8275ad9bdc1f0ebe8b4f1dcba92d
Vitor Mateus <vitor7jan@gmail.com>
2024-09-28 19:12:18 -0300

    added queries

--- queries/views.sql
+++ queries/views.sql
@@ -1,3 +1,4 @@
+-- academic transcripts
 CREATE MATERIALIZED VIEW faculty.academic_transcripts AS
 WITH base AS (
     SELECT
@@ -70,4 +71,63 @@ ORDER BY
     student_name,
     year_semester,
     course_name WITH NO data;
+
+-- courses of degree programs
+CREATE OR REPLACE VIEW faculty.courses_of_degree_programs AS
+SELECT
+    d.degree_program_id AS degree_program_id,
+    d.code AS degree_program_code,
+    d.name AS degree_program_name,
+    c.course_id AS course_id,
+    c.code AS course_code,
+    c.name AS course_name
+FROM
+    faculty.degree_programs d
+    INNER JOIN faculty.degree_programs_courses dc USING (degree_program_id)
+    INNER JOIN faculty.courses c USING (course_id)
+ORDER BY
+    degree_program_name,
+    course_name;
+
+-- curriculums
+CREATE OR REPLACE VIEW faculty.curriculums AS
+WITH stage3 AS (
+    WITH stage2 AS (
+        WITH stage1 AS (
+            SELECT
+                degree_program_id AS degree_program_id,
+                code AS degree_program_code,
+                name AS degree_program_name,
+                unnest(xpath('//curriculum/*', curriculum)) AS curriculum
+            FROM
+                faculty.degree_programs
+)
+            SELECT
+                *,
+                substring((xpath('name(//*[1])', curriculum))[1]::text FROM 8 FOR 2) AS period
+            FROM
+                stage1
+)
+            SELECT
+                degree_program_id,
+                degree_program_code,
+                degree_program_name,
+                period,
+                unnest(xpath('//*/course', curriculum)) AS curriculum
+            FROM
+                stage2
+)
+        SELECT
+            degree_program_id,
+            degree_program_code,
+            degree_program_name,
+            period,
+(xpath('/course/course_code/text()', curriculum))[1]::text AS course_code,
+(xpath('/course/course_name/text()', curriculum))[1]::text AS course_name
+        FROM
+            stage3
+        ORDER BY
+            degree_program_name,
+            period,
+            course_name;
 
